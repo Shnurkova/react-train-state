7a2e0fa0c10f1fffad47f4cde1e5a344a58873b3
Shnurkova <Annshnur@gmail.com>
2024-06-03 21:12:07 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,28 @@
-// import { useState } from "react";
+import { useState } from "react";
 import Description from "../Description/Description";
 import Feedback from "../Feedback/Feedback";
 import Options from "../Options/Options";
 
 export default function App() {
-    let state =  {
+    const startFeedback = { good: 0, neutral: 0, bad: 0 };
+    
+    let state = {
 	good: 0,
 	neutral: 0,
 	bad: 0
     };
-    
-//     const updateFeedback = feedbackType => {
 
-// }
+    const [states, setStates] = useState(() => {
+        const savedStates = window.localStorage.getItem('saved-states');
+        if (savedStates !== null) {
+            return JSON.parse(savedStates);
+        }
+        return { good: 0, neutral: 0, bad: 0 };
+    });
+    
+     const updateFeedback = feedbackType => {
+
+}
 
 
     return (
