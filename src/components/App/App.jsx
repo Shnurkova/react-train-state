dffc97d27e9a8959ee766907ffa8d29524b582c8
Shnurkova <Annshnur@gmail.com>
2024-06-15 18:49:49 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Description from "../Description/Description";
 import Feedback from "../Feedback/Feedback";
 import Options from "../Options/Options";
-import Reader from "./Reader/Reader";
-import articles from "../articles.json"
 
-const ClickCounter = ({value, onCount }) => {   
-    return <button onClick={onCount}>clicks: {value}</button>;
-};
+
 
 export default function App() {
     // const startFeedback = { good: 0, neutral: 0, bad: 0 };
@@ -18,16 +14,6 @@ export default function App() {
 	bad: 0
     };
 
-    const [clicks, setClicks] = useState(0);
-    
-
-    const handleClick = () => {
-        setClicks(clicks + 1)
-    }
-
-    const handleReset = () => {
-        setClicks(0);
-    }
 
 //     const [states, setStates] = useState(() => {
 //         const savedStates = window.localStorage.getItem('saved-states');
@@ -49,13 +35,6 @@ export default function App() {
                 neutral={state.neutral}
                 bad={state.bad}
             />
-            <ClickCounter value={clicks} onCount={ handleClick }/>
-            <ClickCounter value={clicks} onCount={handleClick} />
-            <button onClick={handleReset}>reset</button>
-
-
-
-            <Reader articles={ articles } />
         </>
     );
 }
